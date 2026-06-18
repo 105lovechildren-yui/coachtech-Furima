84c7326cb2ae105b37ed1e45ae644585d773bf50
yui hito <105.love.children@gmail.com>
2026-06-18 12:17:56 +0900

    ER図の変更・マイグレーションファイル作成

---
+++ src/database/migrations/2026_06_12_150410_create_category_item_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_item', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('item_id')->constrained();
+
+            // 同じカテゴリーと商品の組み合わせの重複を防ぐ
+            $table->unique(['category_id', 'item_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_item');
+    }
+}
