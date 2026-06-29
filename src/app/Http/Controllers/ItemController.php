f6762b06fe13f1fcf2175767ff7f6762c1395c17
yui hito <105.love.children@gmail.com>
2026-06-29 11:47:03 +0900

    Fortifyの初期設定とログイン機能の土台を実装

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -7,78 +7,45 @@ use Illuminate\Http\Request;
 class ItemController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * 商品一覧を表示する
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return view('item.index');
     }
 
     /**
-     * Show the form for creating a new resource.
+     * 商品出品フォームを表示する
      *
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('item.create');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * 商品を出品する
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        return redirect()->route('item.index');
     }
 
     /**
-     * Display the specified resource.
+     * 商品詳細を表示する
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        return view('item.show');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
